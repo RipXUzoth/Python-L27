79cb8836e6b70698686fbb63e4f4614d8be0c79f
RipXUzoth <akkuharish1@gmail.com>
2025-06-08 12:09:10 +0530

    hw

---
+++ hw.py
@@ -0,0 +1,12 @@
+import math
+class circle:
+    def __init__(self, radius):
+        self.radius = radius
+        def area(self):
+            return math.pi * self.radius ** 2
+        def perimeter(self):
+            return 2 * math.pi * self.radius
+r = float(input("Enter the radius of the circle: "))
+circle = circle(r)
+print(f"Area of the circle:{circle.area():.2f}")
+print(f"Perimeter  of the circle: {circle.perimeter():.2f}")
